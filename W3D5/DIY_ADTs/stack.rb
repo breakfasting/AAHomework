87fa1af37f958752b05e95ff39776903c02bcc77
breakfasting <breakfast.ing@gmail.com>
2020-02-28 01:50:08 -0800

    finished stack class

--- W3D5/DIY_ADTs/stack.rb
+++ W3D5/DIY_ADTs/stack.rb
@@ -1,17 +1,27 @@
 class Stack
   def initialize
     # create ivar to store stack here!
+    @stack = []
   end
 
   def push(el)
     # adds an element to the stack
+    @stack << el
+    true
   end
 
   def pop
     # removes one element from the stack
+    unless @stack.empty?
+      @stack.pop
+      return true
+    else
+      return false
+    end
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
+    @stack[-1]
   end
 end

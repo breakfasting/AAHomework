fb0476cc31ddd13f911fdadc8105bc730db1805e
breakfasting <breakfast.ing@gmail.com>
2020-03-24 00:16:22 -0700

    finished W7D1 rails auth

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -1,8 +1,34 @@
 class User < ApplicationRecord
   validates :username, :session_token, presence: true
   validates :password_digest, presence: { message: "Password can't be blank" }
-  validates :password_digest, length: { minimum: 7, message: "Password must be longer than 7 chars" }
-  validates :password_digest, allow_nil: true
+  validates :password, length: { minimum: 7, allow_nil: true }
   before_validation :ensure_session_token
 
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if !user.nil? && BCrypt::Password.new(user.password_digest).is_password?(password)
+      return user
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    new_token = User.generate_session_token
+
+    self.update(session_token: new_token)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(val)
+    @password = val
+    self.password_digest = BCrypt::Password.create(val)
+  end
 end

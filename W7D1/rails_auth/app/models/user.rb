cdcc20e8b5c92d8d7a76565602c5e0028f7a6ac2
breakfasting <breakfast.ing@gmail.com>
2020-03-23 23:40:01 -0700

    finished validations

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password_digest, length: { minimum: 7, message: "Password must be longer than 7 chars" }
+  validates :password_digest, allow_nil: true
+  before_validation :ensure_session_token
+
 end

c06e6ccee9eb6cd6a0438c3bc80cd1cfb416d589
breakfasting <breakfast.ing@gmail.com>
2020-04-02 00:56:05 -0700

    finished W8D4 til paradeHelper

---
+++ W8D3/JSIntroProblems.js
@@ -0,0 +1,49 @@
+function titleize(names, cb) {
+  let new_names = names.map(el => `Mx. ${el} Jingleheimer Schmidt`);
+  new_names.forEach(cb);
+}
+
+// titleize(["Mary", "Brian", "Leo"], el => console.log(el));
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'"`);
+};
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function() {
+  console.log(this.tricks[Math.floor(Math.random() * this.tricks.length)]);
+};
+
+
+// let ele1 = new Elephant('Dumbo', 23, ['fly']);
+// ele1.trumpet();
+// ele1.addTrick('jump');
+// ele1.play();
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function (ele) {
+  console.log(`${ele.name} is trotting by!`)
+}
+
+// Elephant.paradeHelper(micah);
+
+// herd.forEach(Elephant.paradeHelper);

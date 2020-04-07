f21bbb0382001133884eaf5b9259c433d7680e33
breakfasting <breakfast.ing@gmail.com>
2020-04-06 17:47:15 -0700

    finished canvas practice

---
+++ W9D1/canvas_practice/practice.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  let canvas = document.getElementById('mycanvas');
+  const ctx = canvas.getContext('2d')
+
+  // ctx.fillStyle = "red";
+  // ctx.fillRect(100, 100, 400, 300); 
+
+  // ctx.beginPath();
+  // ctx.arc(250, 250, 250, 0, 360);
+  // ctx.strokeStyle = 'red';
+  // ctx.lineWidth = 1;
+  // ctx.stroke();
+  // ctx.fillStyle = "red";
+  // ctx.fill();
+});
